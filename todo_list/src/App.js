816e60bedeb686f8a85d598776c07c10e76898b5
dhyan <Dhyan.Shetty@ibm.com>
2024-02-05 15:58:52 +0530

    add new todo functionality

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -4,7 +4,25 @@ const App = () => {
   const [todos, setTodos] = useState([]);
   
   // Add the handlesubmit code here
-  
+  function handleSubmit(e){
+    e.preventDefault();
+
+    let todo = document.getElementById("todoAdd").value;
+    const newTodo = {
+      id : new Date().getTime(),
+      text : todo.trim(),
+      completed : false
+    };
+
+    if(newTodo.text.length > 0){
+      setTodos([...todos].concat(newTodo));
+    }
+    else{
+      alert("Enter Valid Task");
+    }
+
+    document.getElementById("todoAdd").value = "";
+  }
   
   // Add the deleteToDo code here
 
@@ -18,10 +36,18 @@ const App = () => {
 return(
 <div className ="App">
 <h1>Todo List</h1>
-<form>
+<form onSubmit={handleSubmit}>
 <input type ="text" align ="right" id= 'todoAdd'/>
 <button type ="submit">Add Todo</button>
 </form>
+{
+  todos.map((todo) => 
+        <div className="todo" key={todo.id}>
+          <div className="todo-text">
+            {todo.text}
+          </div>
+        </div>)
+}
 </div>
 );
 };
