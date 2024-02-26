5dfd420da16bc273ea8bede355ba9152189ae145
dhyan <Dhyan.Shetty@ibm.com>
2024-02-26 15:08:09 +0530

    add completed project files

--- react-redux-master/src/components/DivPanel.js
+++ react-redux-master/src/components/DivPanel.js
@@ -1,1 +1,9 @@
+import React from "react";
+import { useSelector } from "react-redux";
 
+const divPanel = () => {
+  let counterVal = useSelector((state) => state.counter);
+  return <div>This is the current value of the counter : {counterVal}</div>;
+};
+
+export default divPanel;
--- react-redux-master/src/components/MyButton.js
+++ react-redux-master/src/components/MyButton.js
@@ -1,1 +1,12 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import increment from "../actions";
 
+const myButton = () => {
+  let dispatch = useDispatch();
+  return (
+    <button onClick={() => dispatch(increment(1))}>Increment counter</button>
+  );
+};
+
+export default myButton;
